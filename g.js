9cd32e4fe7357b2c5f739b9e2ba2d371fd5f8568
Infernet89 <infernet2002@libero.it>
2018-08-19 19:31:15 +0200

    Working third level, and something

--- g.js
+++ g.js
@@ -12,6 +12,7 @@ var activeTask;
 var level=0;
 var animationProgress=0;
 var solutionObject=null;
+var randomPlaces=null;
 
 //mobile controls
 var mousex=-100;
@@ -21,9 +22,11 @@ var dragging=false;
 //setup
 canvas = document.getElementById("g");
 ctx = canvas.getContext("2d");
-canvasW=canvas.width  = window.innerWidth;
-canvasH=canvas.height = window.innerHeight;
-canvasW=(canvasH/800)*1200;
+canvasW=canvas.width  = 1200;//window.innerWidth;
+canvasH=canvas.height = 800;//window.innerHeight;
+//canvasW=(canvasH/800)*1200;
+maxDistance=Math.sqrt(canvasW*canvasW+canvasH*canvasH)/4;
+maxIntensity=7;
 
 //controls
 canvas.addEventListener("mousemove",mossoMouse);
@@ -31,72 +34,120 @@ canvas.addEventListener("mousedown",cliccatoMouse);
 canvas.addEventListener("mouseup",rilasciatoMouse);
 window.addEventListener('keyup',keyUp,false);
 
-level=3;//TODO change level here
+level=0;//TODO change level here
 generateLevel();
 activeTask=setInterval(run, 33);
 
 function generateLevel()
 {
+    animationProgress=0;
+    solutionObject=new Object();
+    solutionObject.hasRectangle=true;
+    solutionObject.offsetY=0;
+    solutionObject.alpha=1;
+    solutionObject.color="#0F0";
     if(level==0)
     {
-        solutionObject=new Object();
+
         solutionObject.sizeX=60;
         solutionObject.sizeY=60;
         solutionObject.x=canvasW/2-30
         solutionObject.y=canvasH-120;
-        solutionObject.alpha=1;
     }
     else if(level==1)
     {
-        animationProgress=0;
-        solutionObject=new Object();
-        solutionObject.sizeX=260;
+        
+        solutionObject.sizeX=300;
         solutionObject.sizeY=65;
-        solutionObject.x=355
-        solutionObject.y=300;
-        solutionObject.alpha=1;
+        solutionObject.x=455
+        solutionObject.y=450;
+        solutionObject.alpha=0.2;
+        solutionObject.hasRectangle=false;
+    }
+    else if(level==2)
+    {
+        solutionObject.sizeX=40;
+        solutionObject.sizeY=40;
+        solutionObject.x=10
+        solutionObject.y=80;
+        solutionObject.offsetY=-18;
+        solutionObject.hasRectangle=false;
+        randomPlaces=[];
+        for(i=0;i<canvasW/40;i++)
+        {
+            randomPlaces[i]=[];
+            for(k=0;k<-5+canvasH/40;k++)
+            {
+                randomPlaces[i][k]=new Object();
+                randomPlaces[i][k].char=String.fromCharCode(rand(33,255));
+                r=rand(0,2)
+                if(r==2)
+                    randomPlaces[i][k].color="#FFF"; 
+                else if(r==1)
+                    randomPlaces[i][k].color="#F00"; 
+                else randomPlaces[i][k].color="#0F0"; 
+            }
+                
+        }
+        r=rand(0,2)
+        if(r==2)
+            solutionObject.color="#FFF"; 
+        else if(r==1)
+            solutionObject.color="#F00"; 
+        else solutionObject.color="#0F0";
+        solX=rand(0,-1+canvasW/40);
+        solY=rand(0,-6+canvasH/40);
+        randomPlaces[solX][solY].char="";
+        solutionObject.x=solX*40;
+        solutionObject.y=canvasH-solY*40-10;
+        //ctx.fillText(randomPlaces[i][k].char,i*40,canvasH-k*40-10);
     }
 }
 function drawSolutionPoint(o)
 {
-    ctx.fillStyle="#0F0";
-    ctx.fillRect(o.x,o.y,o.sizeX,2);
-    ctx.fillRect(o.x,o.y-o.sizeY,o.sizeX,2);
-    ctx.fillRect(o.x,o.y-o.sizeY,2,o.sizeY);
-    ctx.fillRect(o.x+o.sizeX,o.y-o.sizeY,2,o.sizeY+2);
+    ctx.save();
+    ctx.globalAlpha=o.alpha;
+    ctx.fillStyle=o.color;
+    if(o.hasRectangle)
+    {
+        ctx.fillRect(o.x,o.y,o.sizeX,2);
+        ctx.fillRect(o.x,o.y-o.sizeY,o.sizeX,2);
+        ctx.fillRect(o.x,o.y-o.sizeY,2,o.sizeY);
+        ctx.fillRect(o.x+o.sizeX,o.y-o.sizeY,2,o.sizeY+2);
+    }
 
     ctx.beginPath();
-    ctx.arc(o.x+o.sizeX/2, o.y-25, 5, 0, 2 * Math.PI, false);
+    ctx.arc(o.x+o.sizeX/2, o.y-25-o.offsetY, 5, 0, 2 * Math.PI, false);
     ctx.fill();
     ctx.lineWidth = 5/10;
-    ctx.strokeStyle = '#0F0';
+    ctx.strokeStyle = o.color;
     ctx.stroke();
     ctx.closePath();
     for(i=0;i<5;i++)
     {
         ctx.beginPath();
-        ctx.arc(o.x+o.sizeX/2, o.y-25, 5+i*5, 230*TO_RADIANS, 310*TO_RADIANS, false);
+        ctx.arc(o.x+o.sizeX/2, o.y-25-o.offsetY, 5+i*5, 230*TO_RADIANS, 310*TO_RADIANS, false);
         ctx.lineWidth = 1;
-        ctx.strokeStyle = '#0F0';
+        ctx.strokeStyle = o.color;
         ctx.stroke();
         ctx.closePath();
     }
+    ctx.restore();
 }
 function drawDistanceIndicator(to)
 {
     //calcola distanza tra mousex,mousey e to
-    distance=(mousex-to.x-to.sizeX/2)*(mousex-to.x-to.sizeX/2)+(mousey-to.y+to.sizeY/2)*(mousey-to.y+to.sizeY/2);
-    //rendi la distanza un numero da 0 a 5
-    maxDistance=(canvasW*canvasW+canvasH*canvasH)/4;
-    distance=5-5*(distance/maxDistance);
+    distance=Math.sqrt((mousex-to.x-to.sizeX/2)*(mousex-to.x-to.sizeX/2)+(mousey-to.y+to.sizeY/2)*(mousey-to.y+to.sizeY/2));
+    //rendi la distanza un numero da 0 a maxIntensity
+    distance=maxIntensity-maxIntensity*(distance/maxDistance);
     //document.title=distance;
     
     ctx.save();
     //ctx.translate(mousex,mousey);//follow the mouse
-    ctx.translate(canvasW-25,20);//on top-right
-    if(distance<-0.3)
+    ctx.translate(canvasW-30    ,20);//on top-right
+    if(distance<-3)
         ctx.fillStyle="#F00";
-    else if(distance>4.95)
+    else if(distance>maxIntensity-0.8)
         ctx.fillStyle="#0F0";
     else
         ctx.fillStyle="#FFF";
@@ -157,53 +208,63 @@ function run()
     {
         animationProgress++;
         //document.title=animationProgress;
+        if(animationProgress>180) 
+            drawSolutionPoint(solutionObject);
 
         toWrite="This";
         toWrite=toWrite.substring(0,animationProgress/2);
         ctx.fillStyle="#FFF";
         ctx.font="40px Arial";
-        ctx.fillText(toWrite,50,100);
+        ctx.fillText(toWrite,150,250);
 
         toWrite="little";
         toWrite=toWrite.substring(0,-8+animationProgress/2);
         ctx.font="20px Arial";
-        ctx.fillText(toWrite,140,100);
+        ctx.fillText(toWrite,240,250);
 
         toWrite="game";
         toWrite=toWrite.substring(0,-16+animationProgress/2);        
         ctx.font="40px Arial";
-        ctx.fillText(toWrite,190,100);
+        ctx.fillText(toWrite,290,250);
 
         toWrite="is gonna";
         toWrite=toWrite.substring(0,-24+animationProgress/2);  
         ctx.font="40px Arial";
-        ctx.fillText(toWrite,50,180);
+        ctx.fillText(toWrite,150,330);
 
         ctx.fillStyle="#F00";
         ctx.font="90px Arial";
         if(animationProgress>70)        
-            ctx.fillText("Hurt",50,300);
+            ctx.fillText("Hurt",150,450);
 
         ctx.fillStyle="#FFF";
         ctx.font="40px Arial";
         if(animationProgress>90) 
-            ctx.fillText("your",250,300);
+            ctx.fillText("your",350,450);
 
         ctx.fillStyle="#0F0";
         ctx.font="90px Arial";
         if(animationProgress>110) 
-            ctx.fillText("BRAIN!",350,300);
+            ctx.fillText("BRAIN!",450,450);
         else dragging=false;//avoid click while loading animation
-
     }
-    else if(level==3)
+    else if(level==2)
     {
-        ctx.fillStyle="#FFF";
-        ctx.font="25px Webdings";//TODO vedere se windows e mobile ce la fanno lo stesso. Apparentemente sì.
-        ctx.fillText("qwertyuiopasdfghjklzxcvbnm",10,50);
-        ctx.fillText("QWERTYUIOPASDFGHJKLZXCVBNM",10,100);
+        ctx.font="40px Webdings";
+        for(i=0;i<canvasW/40;i++)
+            for(k=0;k<-5+canvasH/40;k++)
+            {
+                ctx.fillStyle=randomPlaces[i][k].color;
+                ctx.fillText(randomPlaces[i][k].char,i*40,canvasH-k*40-10);
+                //ctx.fillText("a",i*40,canvasH-k*40-10);
+            }
+                
+        /*ctx.fillText("QWERTYUIOPASDFGHJKLZXCVBNM",10,100);
         ctx.fillText("1234567890!£$%&/()=?^'ì.:,;",10,150);
         ctx.fillText("<>àèìòù@ç°§*é+][#{}",10,200);
+        ctx.fillText("qwertyuiopasdfghjklzxcvbnm",10,250);*/
+
+        drawSolutionPoint(solutionObject);
     }
     //TODO altri livelli qui
 
@@ -259,15 +320,15 @@ function cliccatoMouse(evt)
 {
     dragging=true;
     var rect = canvas.getBoundingClientRect();
-    mousex=(evt.clientX-rect.left)/(rect.right-rect.left)*window.innerWidth;
-    mousey=(evt.clientY-rect.top)/(rect.bottom-rect.top)*window.innerHeight;
+    mousex=(evt.clientX-rect.left)/(rect.right-rect.left)*canvasW;
+    mousey=(evt.clientY-rect.top)/(rect.bottom-rect.top)*canvasH;
 //< >
 }
 function mossoMouse(evt)
 {
     var rect = canvas.getBoundingClientRect();
-    mousex=(evt.clientX-rect.left)/(rect.right-rect.left)*window.innerWidth;
-    mousey=(evt.clientY-rect.top)/(rect.bottom-rect.top)*window.innerHeight;
+    mousex=(evt.clientX-rect.left)/(rect.right-rect.left)*canvasW;
+    mousey=(evt.clientY-rect.top)/(rect.bottom-rect.top)*canvasH;
 }
 function rilasciatoMouse(evt)
 {
